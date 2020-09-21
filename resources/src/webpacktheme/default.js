7178bf71d3c3a0db2dee414ca5174660c5936a33
Steffen Lindner <mail@steffen-lindner.de>
2020-09-21 09:14:59 +0200

    Add Mosaic to webpack

--- resources/src/webpacktheme/default.js
+++ resources/src/webpacktheme/default.js
@@ -6,10 +6,16 @@ import './theme.less';
 import jQuery from 'jquery';
 import registry from 'pat-registry';
 
+
+import(/* webpackChunkName: "mosaic" */ 'mosaic').then(
+    () => jQuery(($) => setTimeout(() => registry.scan(document.body)))
+);
+
 if (jQuery('body.userrole-anonymous').length === 0) {
     import(/* webpackChunkName: "logged-in" */ './logged-in').then(
     () => jQuery(($) => setTimeout(() => registry.scan(document.body)))
   );
+    import(/* webpackChunkName: "layouts-editor" */ 'layouts-editor');
 }
 if (jQuery('body.userrole-anonymous').length > 0) {
   jQuery(($) => setTimeout(() => registry.scan(document.body)));
